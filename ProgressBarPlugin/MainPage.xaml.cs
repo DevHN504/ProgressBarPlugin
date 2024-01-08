c6f701a88f855b9e5549935a36353861d0acb64e
DevHN504 <155341765+DevHN504@users.noreply.github.com>
2024-01-07 22:07:53 -0600

    Initial commit

---
+++ ProgressBarPlugin/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.LocalNotification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProgressBarPlugin
+{
+    public partial class MainPage : ContentPage
+    {
+        private static int _progress;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ShowNotification(object sender, EventArgs e)
+        {
+            _progress = 0;
+
+            var notificationRequest = new NotificationRequest
+            {
+                NotificationId = new Random().Next(),
+                Title = "Progress Bar",
+                Description = $"{_progress}%",
+                Android = new Plugin.LocalNotification.AndroidOption.AndroidOptions()
+                {
+                    ProgressBarMax = 100,
+                    ProgressBarProgress = 0,
+                    IsProgressBarIndeterminate = false
+                }
+            };
+
+            await LocalNotificationCenter.Current.Show(notificationRequest);
+
+            Device.StartTimer(TimeSpan.FromSeconds(3), () =>
+            {
+                _progress += 10;
+
+                notificationRequest.Android.ProgressBarProgress = _progress;
+                notificationRequest.Description = $"{_progress}%";
+                LocalNotificationCenter.Current.Show(notificationRequest);
+
+                if (_progress == 100)
+                {
+                    return false;
+                }
+
+                return true;
+            });
+        }
+    }
+}
